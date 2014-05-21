22222047d40702dd061f6cadb067bec718401b7d
Kevin Murray <spam@kdmurray.id.au>
2014-05-21 12:53:23 +1000

    [setup] v0.2.4 version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requires = [
 setup(
     name="exif2timestream",
     py_modules=['exif2timestream', ],
-    version="0.2.3",
+    version="0.2.4",
     install_requires=install_requires,
     tests_require=test_requires,
     description=desc,
