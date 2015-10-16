1930f68d3ba1d7764f68303d9eae37e291dd5393
Jordan Braiuka <jordan_braiuka@hotmail.com>
2015-10-16 13:27:28 +1100

    Minor Bugfix

--- DeleteImagesByTime.py
+++ DeleteImagesByTime.py
@@ -116,12 +116,9 @@ def find_image_files(camera):
 def process_image(args):
     log.debug("Starting to process image")
     image, camera, ext = args
-    try:
-        image_date = get_file_date(image, 0, round_secs=1,date_mask=camera.date_mask)
-    except struct.error:
-        pass
     delete = False
     try:
+        image_date = get_file_date(image, 0, round_secs=1,date_mask=camera.date_mask)
         time_tuple = (image_date.tm_hour, image_date.tm_min)
         if image_date is None:
             pass
@@ -160,7 +157,7 @@ def process_image(args):
             except OSError:
                 log.error("Could not delete '{0}'".format(image))
                 log.debug("Deleted {}".format(image))
-    except AttributeError:
+    except (AttributeError, struct.error):
         log.error ("Failed on this image", image)
 
 
